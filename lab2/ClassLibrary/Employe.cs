b5457936acaf439af33e42992a57970e000eb62a
Pivanka <76396262+Pivanka@users.noreply.github.com>
2022-04-27 23:35:33 +0300

    Add files via upload

---
+++ lab2/ClassLibrary/Employe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Employe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int NumberTax { get; set; }
+        public string Education { get; set; }
+        public string Profession { get; set; }
+        public DateTime DateOfStartWork { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name} {Patronymic} {Surname} |" +
+                $" {DateOfBirth.ToShortDateString()} | Tax Number={NumberTax} | Education={Education} | " +
+                $"Profession={Profession} | Date of start={DateOfStartWork.ToShortDateString()} ");
+        }
+    }
+}
---
+++ lab2/ClassLibrary/Salary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Salary
+    {
+        public int Rate { get; set; }
+        public MonthOfYear Month { get; set; }
+        public int Year { get; set; }
+
+        public int NumberTax { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"Rate = {Rate}, Month = {Month.ToString()}\n" +
+                $"Year = {Year}, Tax Number={NumberTax}");
+        }
+    }
+}
